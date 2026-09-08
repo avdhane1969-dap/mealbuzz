6346381579a2082df8eab7bf22ae03b16ccf3fd5
avdhane1969-dap <avdhane1969@gmail.com>
2026-09-09 02:45:02 +0530

    Add lightweight Meal Buzz service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="mealbuzz-v1";
+const CORE=["./index.html","./partner-login.html","./register.html"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{})));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request)))});
