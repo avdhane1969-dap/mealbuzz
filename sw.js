84173c79d15bef810fc116a1ac1d01b13c5e518d
avdhane1969-dap <avdhane1969@gmail.com>
2026-09-09 23:58:24 +0530

    Refresh site service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="mealbuzz-v4";
+const CACHE="mealbuzz-v5";
 const CORE=["./index.html","./assets/hero-food.jpg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
